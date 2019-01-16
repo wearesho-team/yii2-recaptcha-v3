8a8deebfa350cf1b14e9524985812cf94b7fbbe0
Alexander Letnikow <reclamme@gmail.com>
2019-01-16 19:31:22 +0200

    Implement Bootstrap

---
+++ src/Bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Wearesho\ReCaptcha\V3\Yii2;
+
+use yii\base;
+use Wearesho\ReCaptcha;
+
+/**
+ * Class Bootstrap
+ * @package Wearesho\ReCaptcha\V3\Yii2
+ */
+class Bootstrap implements base\BootstrapInterface
+{
+    /** @var array|string|ReCaptcha\V3\ConfigInterface */
+    public $config = [
+        'class' => ReCaptcha\V3\EnvironmentConfig::class,
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap($app): void
+    {
+        \Yii::$container->setSingleton(
+            ReCaptcha\V3\ConfigInterface::class,
+            $this->config
+        );
+    }
+}
